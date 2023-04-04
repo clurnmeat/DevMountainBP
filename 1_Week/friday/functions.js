efd6a191c1712924d37c3d798960bad98b7a8b11
Dionte Walker <walkerdionte@outlook.com>
2023-04-04 16:35:22 -0500

    started functions for wednesday

---
+++ 1_Week/friday/functions.js
@@ -0,0 +1,13 @@
+function myFunction(num) {
+    num += 2;
+};
+
+myFunction(4)
+
+
+function shoeMaker(color, size){
+    console.log(color + ''+ ' size ' + size + '' + ' shoe')
+    return size;
+};
+
+console.log(shoeMaker('red', 10))

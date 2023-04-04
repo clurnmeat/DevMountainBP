c4f836303a86fc0bd2c4a63e1ce44c10680768b7
Dionte Walker <walkerdionte@outlook.com>
2023-04-04 11:57:31 -0500

    getting started

---
+++ 1_Week/tuesday/myfile.js
@@ -0,0 +1,29 @@
+let mix = 34;
+
+if (mix - 4 === 30){
+    console.log("That is correct!");
+} else {
+    console.log("That is incorrect!")
+}
+
+let temperature = 75;
+
+if (temperature === 75) {
+    console.log("Perfect Temp");
+} else {
+    console.log("Not Perfect temp")
+}
+
+const name = `My name is John, I'm ${temperature} degrees today`;
+if (temperature >= 75){
+    console.log(name);
+}
+
+
+
+
+
+
+    
+
+

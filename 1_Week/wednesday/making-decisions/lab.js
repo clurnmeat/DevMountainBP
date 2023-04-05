18203a43edac7ad8b3b5000ca71055e090143c2d
Dionte Walker <walkerdionte@outlook.com>
2023-04-05 18:47:40 -0500

    continued work on python side project, search bar and temp implemented

--- 1_Week/wednesday/making-decisions/lab.js
+++ 1_Week/wednesday/making-decisions/lab.js
@@ -155,3 +155,11 @@ let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+
+while (z > 0){
+  for(i=z;i>0;i--){
+    console.log(i)
+    
+  }
+  z--
+}
--- Python_Side_Projects/weatherapp/main.py
+++ Python_Side_Projects/weatherapp/main.py
@@ -11,27 +11,51 @@ APIKEY = "eb386d51eb16c1f5ca4e882a58bee4ca"
 screen = tk.Tk()
 screen.geometry("300x400")
 screen.title("Weather App")
-screen.resizable(False, False)
+# screen.resizable(False, False)
 photo = tk.PhotoImage(
     file="Python_Side_Projects/weatherapp/7607342.png")
 screen.iconphoto(False, photo)
-
 # Weather Info
-city_name = tk.Text(screen, height=1, font=("Arial", 25))
-city_name.pack()
 
 
-params = {
-    "q": city_name,
-    "appid": APIKEY,
-    "limit": 3
-}
+def get_input():
+    inp = searchbar.get(1.0, "end-1c")
+    params = {
+        "q": inp,
+        "appid": APIKEY,
+        "limit": 3
+    }
+
+    location = requests.get(url=GEOLOCATOR, params=params).json()
+
+    return (location[0]["lat"], location[0]["lon"])
 
 
-location = requests.get(url=GEOLOCATOR, params=params).json()
-with open(location) as file:
-    lat = file["lat"]
-    lon = file["lon"]
-    print(lat, lon)
+def get_weather():
+    location_info = get_input()
+    params2 = {
+        "lat": location_info[0],
+        "lon": location_info[1],
+        "appid": APIKEY,
+        "units": "imperial"
+
+    }
+    response = requests.get(API, params=params2).json()
+    weather = response["main"]["temp"]
+    weather_num = tk.Label(text=weather, font=("Arial", 45))
+    weather_num.grid(row=2, column=0, padx=25, pady=50)
+    return
+
+
+# Search Button
+searchbutton = tk.Button(text="Search", height=2, command=get_weather)
+searchbutton.grid(row=0, column=1)
+
+# Search Bar
+searchbar = tk.Text(height=1, font=("Arial", 25), width=12)
+searchbar.grid(row=0, column=0,)
+
+# Weather Label
+
 
 screen.mainloop()

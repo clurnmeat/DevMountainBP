e97ec34e23a92d003b3daa9d52b70cfd90595656
Dionte Walker <walkerdionte@outlook.com>
2023-04-05 11:37:34 -0500

    updated notes with for-loops for wednesday

--- 1_Week/wednesday/notes.js
+++ 1_Week/wednesday/notes.js
@@ -1,6 +1,16 @@
 const new_list = []
 for (i = 0; i < 50; i++ ){
-    new_list.push(i)
+    new_list.push(i);
+
+};
+
+
+
+const list = [0, 1, 2, 3, 4]
+for (i = 0; i < 50; i++) {
+    if ( list[i] !== (i) ) {
+        list.push(i)
+    }
 
 }
-console.log(new_list)
+console.log(list)

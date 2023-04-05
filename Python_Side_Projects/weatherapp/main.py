1643de4a1dfc6811602e7107aedb415b4bca7fe1
Dionte Walker <walkerdionte@outlook.com>
2023-04-05 11:10:20 -0500

    updated weather app using python

--- Python_Side_Projects/weatherapp/main.py
+++ Python_Side_Projects/weatherapp/main.py
@@ -1,17 +1,34 @@
-from tkinter import *
+import tkinter as tk
 import requests
 
 
-API = "https://api.openweathermap.org/data/3.0/onecall"
-
+GEOLOCATOR = "http://api.openweathermap.org/geo/1.0/direct"
+API = "https://api.openweathermap.org/data/2.5/weather"
 APIKEY = "eb386d51eb16c1f5ca4e882a58bee4ca"
 
+
 # Screen
-screen = Tk()
+screen = tk.Tk()
 screen.geometry("300x400")
 screen.title("Weather App")
 screen.resizable(False, False)
-screen.iconbitmap(
-    'Python_Side_Projects/weatherapp/images/pythontutorial-1-150x150.ico')
+photo = tk.PhotoImage(
+    file="Python_Side_Projects/weatherapp/7607342.png")
+screen.iconphoto(False, photo)
+
+# Weather Info
+city_name = tk.Text(screen)
+city_name.pack()
+
+
+params = {
+    "q": city_name,
+    "appid": APIKEY,
+    "limit": 3
+}
+
+
+location = requests.get(url=GEOLOCATOR, params=params).json()
+
 
 screen.mainloop()

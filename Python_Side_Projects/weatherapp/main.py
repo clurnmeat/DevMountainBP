0ce7884cfd4d01ff5de83d5e3cf03f2cdd8ad574
Dionte Walker <walkerdionte@outlook.com>
2023-04-06 14:34:45 -0500

    completed exercise

--- Python_Side_Projects/weatherapp/main.py
+++ Python_Side_Projects/weatherapp/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import requests
-
+from PIL import ImageTk, Image
 
 GEOLOCATOR = "http://api.openweathermap.org/geo/1.0/direct"
 API = "https://api.openweathermap.org/data/2.5/weather"
@@ -9,12 +9,16 @@ APIKEY = "eb386d51eb16c1f5ca4e882a58bee4ca"
 
 # Screen
 screen = tk.Tk()
-screen.geometry("300x400")
+screen.geometry("400x250")
 screen.title("Weather App")
 screen.resizable(False, False)
-photo = tk.PhotoImage(
-    file="weatherapp/7607342.png")
-screen.iconphoto(False, photo)
+background = ImageTk.PhotoImage(Image.open(
+    "Python_Side_Projects/weatherapp/th-920371034.png"))
+image = tk.Label(screen, image=background)
+image.grid(row=5, column=0, rowspan=8, columnspan=8)
+appico = tk.PhotoImage(
+    file="Python_Side_Projects/weatherapp/7607342.png")
+screen.iconphoto(False, appico)
 # Weather Info
 
 
@@ -43,8 +47,9 @@ def get_weather():
     # Weather Label
     response = requests.get(API, params=params2).json()
     weather = response["main"]["temp"]
-    weather_num = tk.Label(text=weather, font=("Arial", 45))
-    weather_num.grid(row=2, column=0, padx=65, pady=125, columnspan=4)
+    weather_num = tk.Label(text=weather, font=("Arial", 45), relief="flat")
+    weather_num.grid(row=2, column=0,
+                     rowspan=4, pady=80, )
     return
 
 
@@ -53,7 +58,7 @@ searchbutton = tk.Button(text="Search", height=2, command=get_weather)
 searchbutton.grid(row=0, column=1)
 
 # Search Bar
-searchbar = tk.Text(height=1, font=("Arial", 25), width=12, )
+searchbar = tk.Text(height=1, font=("Arial", 28), width=12, )
 searchbar.grid(row=0, column=0, )
 
 

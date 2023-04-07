e75423e9703fc9890ce48a73882e05ccb5a2888a
Dionte Walker <walkerdionte@outlook.com>
2023-04-07 14:57:54 -0500

    update

---
+++ 1_Week/friday/assessments/assessment-js-basics-1/sdlc.js
@@ -0,0 +1,31 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning =
+  "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+const analysis =
+  "Once you've gathered your ideas, it's time to organize them into a cohesive plan and design. This requires a lot of research and planning to ensure that your final product meets your expectations (and those of your customers). The big step is creating a detailed project plan document and work breakdown structure that outline everything from user experience goals to database structure requirements.";
+const design =
+  "Once you've got your design plans in front of you, it's time for wireframing and mockups so that everyone involved knows exactly how their part will fit into the larger design picture. You have many tools available that make this process much easier than ever before. This step builds upon the planning stage, building out the tasks you need to do in the work breakdown schedule.";
+const implementation =
+  "The development phase is where coding begins to take place and is one of the most time-consuming phases in the software lifecycle. This phase often requires extensive programming skills and knowledge of databases and your team will build functionality for your product or service. This includes creating an interface that users interact with and building the database so users can store information in your system.";
+const testIntegration =
+  "Before releasing it into production, you'll need to test it to ensure it is free of bugs and errors. If you find problems, you need to fix them before moving forward with deployment. You'll also need to manage how the system will integrate into existing systems, software, and processes.";
+const maintain =
+  "The final stage of the software development life cycle is maintenance and operations. This is one of the most critical stages because it's when your hard work gets put to the test.";

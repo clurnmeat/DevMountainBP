f96309ef57329dd32f812c2d63bee3b185205fb4
Dionte Walker <walkerdionte@outlook.com>
2023-04-10 09:36:56 -0500

    completed addtozero

--- 2_Week/monday/challenges/challenge-add-to-zero/addToZero.js
+++ 2_Week/monday/challenges/challenge-add-to-zero/addToZero.js
@@ -2,3 +2,14 @@
 let array = [28, 43, -12, 30, 4, 0, 12];
 
 // Write your solution below
+temp = false;
+for (let i = 0; i < array.length; i++) {
+  for (let j = 1; j < array.length; j++) {
+    if (array[i] + array[j] === 0) {
+      temp = true;
+    } else {
+      temp = false;
+    }
+  }
+}
+console.log(temp);

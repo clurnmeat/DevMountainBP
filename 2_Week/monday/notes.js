0a7f41131cd67da188b81402af2749bfb1121343
Dionte Walker <walkerdionte@outlook.com>
2023-04-10 11:55:51 -0500

    started functions-demo

--- 2_Week/monday/notes.js
+++ 2_Week/monday/notes.js
@@ -1,3 +1,15 @@
 let myStrin = "this is a string";
 
 console.log(myStrin[0]);
+for (let i = 0; i < myStrin.length; i++) {
+  console.log(myStrin[i]);
+}
+
+let age = 21;
+
+function logDetails() {
+  let name = "Tyler";
+  return name;
+}
+
+console.log(`${logDetails()}`);

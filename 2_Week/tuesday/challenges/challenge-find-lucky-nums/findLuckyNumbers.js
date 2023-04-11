a39d42061b64946290b3135db4514c839384d446
Dionte Walker <walkerdionte@outlook.com>
2023-04-11 12:49:20 -0500

    completed demo

--- 2_Week/tuesday/challenges/challenge-find-lucky-nums/findLuckyNumbers.js
+++ 2_Week/tuesday/challenges/challenge-find-lucky-nums/findLuckyNumbers.js
@@ -1,12 +1,35 @@
 // Write your code below this line.
-const luckyNumbers = (n) => {
-  setOfNumbers = [];
-  while (setOfNumbers.length < n) {
-    let number = Math.floor(Math.random() * 10) + 1;
-    if (!setOfNumbers.includes(n)) {
-      setOfNumbers.push(n);
+// const luckyNumbers = (n) => {
+//   setOfNumbers = [];
+//   while (setOfNumbers.length < n) {
+//     let number = Math.floor(Math.random() * 10) + 1;
+//     if (!setOfNumbers.includes(n)) {
+//       setOfNumbers.push(n);
+//     }
+//   }
+//   return setOfNumbers;
+// };
+// console.log(luckyNumbers(8));
+const setOfNumbers = [];
+function luckyNumbers(n) {
+  for (let i = 1; i <= n; i++) {
+    number = Math.floor(Math.random() * 10) + 1;
+
+    if (!setOfNumbers.includes(number)) {
+      setOfNumbers.push(number);
+    } else {
+      numbger = Math.floor(Math.random() * 10) + 1;
+      setOfNumbers.push(numbger);
     }
   }
-  return setOfNumbers;
-};
-console.log(luckyNumbers(8));
+}
+
+function setONumbers(n) {
+  let number = Math.floor(Math.random() * 10) + 1;
+  while (!setOfNumbers.includes(number)) {
+    for (let i = 1; i <= n; i++) {
+      setOfNumbers.push(number);
+    }
+  }
+}
+console.log(setONumbers(8));

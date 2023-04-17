33e9f68450f8d4ca7e89f71e87c5cc9130079b81
Dionte <walkerdionte@outlook.com>
2023-04-17 14:03:17 +0000

    completed hasmorevowels.js

---
+++ 3_Week/challenge-has-more-vowels/hasMoreVowels.js
@@ -0,0 +1,23 @@
+// Write a function called hasMoreVowels that takes in one argument, word. When the function is called, return true if that word contains more vowels than non-vowels; otherwise, return false. The word will always be a single word, without any punctuation or spaces. It will contain only uppercase and/or lowercase letters. If the phrase is over half vowels, it should return true:
+
+const hasMoreVowels = (word) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const wrdArr = word.split("");
+  let counter = 0;
+
+  wrdArr.forEach((el) => {
+    if (vowels.includes(el.toLowerCase())) {
+      counter++;
+    }
+  });
+
+  if (counter >= wrdArr.length / 2){
+    return true
+  }else {
+    return false
+  };
+};
+
+
+console.log(hasMoreVowels(`apple`))
+

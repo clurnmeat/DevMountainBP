56e342aa0ebf68d11e774da9d3ac509e4ee7f2e4
Dionte <walkerdionte@outlook.com>
2023-04-19 16:49:43 +0000

    updated, added features to js.demo

---
+++ 3_Week/wednesday/demo/js-interactivity-demo/scripts.js
@@ -0,0 +1,34 @@
+console.log("JS file connected successfully")
+
+
+let count = 0;
+let counter = document.querySelector('#counter')
+let plusBtn = document.querySelector('#plus-btn')
+let minusBtn = document.querySelector('#minus-btn')
+let resetBtn = document.querySelector('#reset-btn')
+
+
+
+let increment = () => {
+    count++;
+    counter.textContent = count;
+}
+
+plusBtn.addEventListener('click', increment)
+
+
+
+let decrement = () => {
+    count--;
+    counter.textContent = count;
+}
+
+minusBtn.addEventListener('click', decrement)
+
+let resetCount = () => {
+    count = 0;
+    counter.textContent = count
+}
+
+resetBtn.addEventListener('click', resetCount)
+

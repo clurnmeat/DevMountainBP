955b8a61dc5ca96760208b6ee2566fe46269c4d7
Dionte Walker <walkerdionte@outlook.com>
2023-04-21 15:21:52 -0500

    updated index git hub clone

---
+++ 3_Week/notes.js
@@ -0,0 +1,15 @@
+const isANum = (num) =>
+  typeof num === "number" ? console.log(true) : console.log(false);
+
+isANum(3);
+
+if (typeof num === "number") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+switch (a) {
+  case "a":
+    console.log(a);
+}

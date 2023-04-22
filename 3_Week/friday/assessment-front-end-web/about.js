47ee8fa3b658dc290e9f965d26d073be260bf827
Dionte <walkerdionte@outlook.com>
2023-04-22 12:53:57 +0000

    updated assessment

--- 3_Week/friday/assessment-front-end-web/about.js
+++ 3_Week/friday/assessment-front-end-web/about.js
@@ -8,8 +8,12 @@ function handleSubmit(evt) {
 }
 let count = 0;
 giveCompliment = () => {
+	let compliment = "";
+	const catpic = document.querySelector("#cat-pic");
 	const compliment_list = document.querySelectorAll(".compliments");
-	const compliment = compliment_list[Math.floor(Math.random() * compliment_list.length)].textContent;
+	for (let i = 0; i < compliment_list.length; i++) {
+	  compliment = compliment_list[Math.floor(Math.random() * compliment_list.length)].textContent;
+	}
 	
 	alert(compliment);
 };
@@ -17,10 +21,3 @@ giveCompliment = () => {
 catpic.addEventListener("mouseover", giveCompliment);
 let form = document.querySelector("#contact");
 form.addEventListener("submit", handleSubmit);
-
-// let compliment = "";
-// const catpic = document.querySelector("#cat-pic");
-// const compliment_list = document.querySelectorAll(".compliments");
-// for (let i = 0; i < compliment_list.length; i++) {
-//   compliment = compliment_list[i].textContent;
-// }

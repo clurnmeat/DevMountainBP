ec49cb558cac3cd29cf4f72dcb2b245f8baaed11
Dionte Walker <walkerdionte@outlook.com>
2023-04-22 07:48:38 -0500

    updated assessment

--- 3_Week/friday/assessment-front-end-web/about.js
+++ 3_Week/friday/assessment-front-end-web/about.js
@@ -1,23 +1,26 @@
-const compliment_list = document.querySelectorAll('.compliments')
-const catpic = document.querySelector('#catpic')
 console.log("hello world");
-
+const catpic = document.querySelector('#cat-pic');
 
 function handleSubmit(evt) {
-	evt.preventDefault();
+  evt.preventDefault();
+
+  alert("Form Submitted!");
+}
+let count = 0;
+giveCompliment = () => {
+	const compliment_list = document.querySelectorAll(".compliments");
+	const compliment = compliment_list[Math.floor(Math.random() * compliment_list.length)].textContent;
 	
-	alert("Form Submitted!");
-}
+	alert(compliment);
+};
 
+catpic.addEventListener("mouseover", giveCompliment);
+let form = document.querySelector("#contact");
+form.addEventListener("submit", handleSubmit);
 
-giveCompliment = (event) => {
-	alert(compliment_list.forEach((el, index) => el[index].textContent))
-}
-
-
-
-
-let form = document.querySelector('#contact');
-
-form.addEventListener('submit', handleSubmit);
-catpic.addEventListener('mouseover', giveCompliment)
+// let compliment = "";
+// const catpic = document.querySelector("#cat-pic");
+// const compliment_list = document.querySelectorAll(".compliments");
+// for (let i = 0; i < compliment_list.length; i++) {
+//   compliment = compliment_list[i].textContent;
+// }

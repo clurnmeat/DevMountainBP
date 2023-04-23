7e44ebfd0e56837306d6eb3046d453369b6daf1d
Dionte <walkerdionte@outlook.com>
2023-04-23 16:34:17 +0000

    completed assessment officially

--- 3_Week/friday/assessment-front-end-web/profile.js
+++ 3_Week/friday/assessment-front-end-web/profile.js
@@ -1,7 +1,25 @@
-const colorBtn = document.querySelector('#color')
-const placeBtn = document.querySelector('#place')
-const ritualBtn = document.querySelector('#ritual')
+const btnList = document.querySelectorAll("button");
 
+const favColorFunc = (event) => {
+  alert(`${event.target.textContent} is Green`);
+};
+const favPlaceFunc = (event) => {
+  alert(`${event.target.textContent} is Minnesota`);
+};
+const favRitualFunc = (event) => {
+  alert(`${event.target.textContent} is making tea`);
+};
 
+btns = Array.from(btnList);
 
+btns.forEach((el, idx) => {
+  if (idx === 0) {
+    el.addEventListener("click", favColorFunc);
+  } else if (idx === 1) {
+    el.addEventListener("click", favPlaceFunc);
+  } else if (idx === 2) {
+    el.addEventListener("click", favRitualFunc);
+  }
+});
 
+console.log(btnList);

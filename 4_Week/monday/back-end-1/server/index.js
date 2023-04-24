7fadb58b5b2f91fc9baf1472130c3c9f740fd100
Dionte <walkerdionte@outlook.com>
2023-04-24 18:57:06 +0000

    update

--- 4_Week/monday/back-end-1/server/index.js
+++ 4_Week/monday/back-end-1/server/index.js
@@ -19,10 +19,5 @@ app.get("/weather/:temperature", (req, res) => {
   res.status(200).send(phrase);
 })
 
-app.get("/weather/:temperature", (req, res) => {
-  const { temperature } = req.params;
-  const phrase = `<h3>It was ${temperature} today</h3>`;
-  res.status(200).send(phrase);
-});
 
 app.listen(4000, () => console.log(`Server is running on 4000`))

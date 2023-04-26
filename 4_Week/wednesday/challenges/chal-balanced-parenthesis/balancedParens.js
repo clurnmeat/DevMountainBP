fe7ad8978fb85e9eb514f61f416e514713957935
Dionte <walkerdionte@outlook.com>
2023-04-26 16:00:33 +0000

    update challenges

--- 4_Week/wednesday/challenges/chal-balanced-parenthesis/balancedParens.js
+++ 4_Week/wednesday/challenges/chal-balanced-parenthesis/balancedParens.js
@@ -4,4 +4,38 @@ let sample2 = "(This (is (a) balanced) string.)"
 let sample3 = "This is () also ) unbalanced."
 let sample4 = "Balanced."
 
-// Write your solution below:
+
+let leftParen = []
+let rightParen = []
+ 
+const balanceFunct = (strng) => {
+    let leftParen = []
+    let rightParen = []
+
+    strng.split('').forEach(element => {
+        if (element == "(") {
+            leftParen.push(element)
+        }
+        else if (element == ")") {
+            rightParen.push(element)
+        }
+    });
+
+    let length = leftParen.length + rightParen.length
+
+    if (leftParen.length % 2 === 0 && rightParen.length % 2 === 0) {
+        return `Balanced`
+    } 
+    else if (length === 0) {
+        return `Balanced`
+    } 
+    else {
+        return `Unbalanced`
+    }
+}
+
+//Test case using console.log().
+console.log(balanceFunct('(('))
+
+
+

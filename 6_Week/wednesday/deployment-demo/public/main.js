ac6df9e7b0b2a409bd4f278eb60221de8ee5298b
clownmeat <reenyobby@yahoo.com>
2023-05-10 18:58:20 +0000

    update

--- 6_Week/wednesday/deployment-demo/public/main.js
+++ 6_Week/wednesday/deployment-demo/public/main.js
@@ -1,5 +1,11 @@
 const btn = document.querySelector('button')
+const dogbtn = document.querySelector('.dog')
+
 
 const clickHandler = () => alert('The cat name is: Rascal')
+const dogclickHandler = () => alert('The dogs name is Rascal')
+
+
 
 btn.addEventListener('click', clickHandler)
+dogbtn.addEventListener('click', dogclickHandler)

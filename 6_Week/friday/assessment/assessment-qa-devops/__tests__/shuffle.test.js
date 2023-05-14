9e30245d87f4d232f5f5159865b7f5fa0ec98967
clownmeat <reenyobby@yahoo.com>
2023-05-14 01:19:38 +0000

    tests created for assessment

--- 6_Week/friday/assessment/assessment-qa-devops/__tests__/shuffle.test.js
+++ 6_Week/friday/assessment/assessment-qa-devops/__tests__/shuffle.test.js
@@ -16,6 +16,35 @@ afterEach(async () => {
 describe("shuffle should...", () => {
   // CODE HERE
   test('Display 5 bots', async () => {
-
+    await driver.get('http://localhost:3000/');
+    let drawBtn = await driver.findElement(By.id("draw"));
+    await drawBtn.click();
+    let botsList = await driver.findElements(By.className("bot-card outline"));
+    expect(botsList.length).toBe(5);
+  }),
+  test('Bots Should be different', async () => {
+    await driver.get('http://localhost:3000/');
+    let drawBtn = await driver.findElement(By.id('draw'));
+    await drawBtn.click();
+    let bots = await driver.findElements(By.css("div.bot-card.outline"));
+    let botsListsNames = [];
+    for(let i=0; i<bots.length; i++){
+      await bots[i].getText().then((text) => {
+        botsListsNames.push((text))
+        
+      });
+    };
+    await driver.get('http://localhost:3000/');
+    let newBotsListNames = [];
+    drawBtn = await driver.findElement(By.id('draw'))
+    drawBtn.click()
+    bots = await driver.findElements(By.css("div.bot-card.outline"));
+    for(let i=0; i<bots.length; i++){
+      await bots[i].getText().then((text) => {
+        newBotsListNames.push(text)
+      })
+    };
+    expect(botsListsNames).not.toBe(newBotsListNames)    
+    
   })
 });
